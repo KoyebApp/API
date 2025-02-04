9574f84d316f2db728ca36ffc138651a27e627eb
Koyeb App <koyebapp@gmail.com>
2025-02-04 17:43:14 +0500

    Update apiRoutes.py

--- routes/apiRoutes.py
+++ routes/apiRoutes.py
@@ -1,1 +1,10 @@
+# /routes/apiRoutes.py
+from flask import Blueprint
+from controllers.apiController import get_data, post_data
+
+api_routes = Blueprint('api_routes', __name__)
+
+# Define the routes
+api_routes.route('/api/get', methods=['GET'])(get_data)
+api_routes.route('/api/post', methods=['POST'])(post_data)
 
