78d6fc29af632692c202e65c14bf6c10422e44bf
Koyeb App <koyebapp@gmail.com>
2025-02-04 17:44:12 +0500

    Update apiController.py

--- controllers/apiController.py
+++ controllers/apiController.py
@@ -1,1 +1,17 @@
+# /controllers/apiController.py
+from flask import request, jsonify
 
+# Handler for GET requests
+def get_data():
+    return jsonify({"message": "This is a GET response"})
+
+# Handler for POST requests
+def post_data():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid JSON"}), 400
+    return jsonify({
+        "message": "This is a POST response",
+        "data": data
+    })
+  
